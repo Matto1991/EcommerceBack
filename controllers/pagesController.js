fcf2853c9801ffefea59f341c1f987afa6aff7a2
Ali <dev.alifm@gmail.com>
2023-03-21 22:58:35 -0300

    Update pagesController.js

--- controllers/pagesController.js
+++ controllers/pagesController.js
@@ -19,8 +19,7 @@
 const { Article } = require("../models");
 
 async function showHome(req, res) {
-  const articles = await Article.findAll();
-  res.render("home", { articles });
+  res.json("Funciona-HOME!");
 }
 
 async function showContact(req, res) {
