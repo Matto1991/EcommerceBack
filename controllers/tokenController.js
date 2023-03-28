6040df0e7b7a44895ae27994eba6f575ec6cdce4
Virginia Barbaro <virginiabarbaro@icloud.com>
2023-03-28 00:54:40 -0300

    jwt

--- controllers/tokenController.js
+++ controllers/tokenController.js
@@ -1,23 +1,34 @@
-const User = require("../models/User");
+const { User } = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 async function token(req, res) {
   try {
-    const user = await User.findOne({ where: { email: req.body.email } });
+    const email = req.body.email;
+    console.log(email);
+    if (!email) {
+      return res.json("email requerida");
+    }
+
+    const user = await User.findOne({ where: { email: email } });
+    console.log("user encontrado", user);
+
     if (user) {
       const checkJwt = await user.isValidPassword(req.body.password);
-
+      console.log(checkJwt)
       const token = jwt.sign({ id: user.id }, "secretKey");
 
-      if (user && checkJwt) {
+      if (checkJwt) {
         return res.json({ token, id: user.id });
-      } else return res.json({ message: "El usuario no existe " });
-    } else return res.json("No se pudo loguear");
+      } else {
+        return res.json( "La pass es incorrecta");
+      }
+    } else {
+      return res.json( "El user no existe");
+    }
   } catch (err) {
     console.log(err);
   }
 }
 
-module.exports = {
-  token,
-};
+
+module.exports = { token };
